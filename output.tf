5b25501da9de52decebd3eba12cab76f99d2bb56
sagark <131134538+sagark4578@users.noreply.github.com>
2025-03-11 15:50:54 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -10,26 +10,10 @@ output "efs_access_point_id" {
   value = aws_efs_access_point.aws_efs_access_point.id
 }
 
-output "efs_id_pgadmin" {
-  value = aws_efs_file_system.aws_efs_file_system_pgadmin.id
+output "efs_id_app2" {
+  value = aws_efs_file_system.aws_efs_file_system_app2.id
 }
 
-output "efs_access_point_pgadmin_id" {
-  value = aws_efs_access_point.aws_efs_access_point_pgadmin.id
-}
-
-output "efs_sonarqube_id" {
-  value = var.environment == "dev" ? aws_efs_file_system.aws_efs_file_system_sonarqube[0].id : null
-}
-
-output "efs_access_point_sonarqube_data_id" {
-  value = var.environment == "dev" ? aws_efs_access_point.aws_efs_access_point_sonarqube_data[0].id : null
-}
-
-output "efs_access_point_sonarqube_logs_id" {
-  value = var.environment == "dev" ? aws_efs_access_point.aws_efs_access_point_sonarqube_logs[0].id : null
-}
-
-output "efs_access_point_sonarqube_extensions_id" {
-  value = var.environment == "dev" ? aws_efs_access_point.aws_efs_access_point_sonarqube_extensions[0].id : null
+output "efs_access_point_app2_id" {
+  value = aws_efs_access_point.aws_efs_access_point_app2.id
 }
